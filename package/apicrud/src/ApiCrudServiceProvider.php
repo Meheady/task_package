f55bb5eeb32ff75de8988277aaa78828cb538bea
Meheady <64876349+Meheady@users.noreply.github.com>
2024-06-12 18:51:59 +0600

    dfd

---
+++ package/apicrud/src/ApiCrudServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Package\Apicrud;
+
+use Illuminate\Support\ServiceProvider;
+
+class ApiCrudServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register():
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot():
+    {
+        //
+    }
+}
